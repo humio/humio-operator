51f1f9a06e6b2819ed92a923b7d669282fe4475e
Jestin Woods <jestin@humio.com>
2020-02-25 15:16:32 -0800

    Add basic example test

---
+++ pkg/controller/humiocluster/humiocluster_controller_test.go
@@ -0,0 +1,86 @@
+package humiocluster
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	humioClusterv1alpha1 "github.com/humio/humio-operator/pkg/apis/core/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var (
+	humioCluster1 = &humioClusterv1alpha1.HumioCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "humiocluster",
+			Namespace: "logging",
+		},
+		Spec: humioClusterv1alpha1.HumioClusterSpec{
+			Image:                   "humio/humio-core",
+			Version:                 "1.9.0",
+			TargetReplicationFactor: 1,
+		},
+	}
+)
+
+func TestReconcileHumioCluster_Reconcile(t *testing.T) {
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(logf.ZapLogger(true))
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		humioCluster1,
+	}
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(humioClusterv1alpha1.SchemeGroupVersion, humioCluster1)
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	// Create a ReconcileHumioCluster object with the scheme and fake client.
+	r := &ReconcileHumioCluster{client: cl, scheme: s}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      humioCluster1.ObjectMeta.Name,
+			Namespace: humioCluster1.ObjectMeta.Namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	pod := &corev1.Pod{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: fmt.Sprintf("%s-pod", humioCluster1.ObjectMeta.Name), Namespace: humioCluster1.ObjectMeta.Namespace}, pod)
+	if err != nil {
+		t.Fatalf("get pod: (%v). %+v", err, pod)
+	}
+
+	// Reconcile again so Reconcile() checks pods and updates the HumioCluster resources' Status.
+	res, err = r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	if res != (reconcile.Result{}) {
+		t.Error("reconcile did not return an empty Result")
+	}
+
+	// Get the updated HumioCluster object.
+	humioCluster := &humioClusterv1alpha1.HumioCluster{}
+	err = r.client.Get(context.TODO(), req.NamespacedName, humioCluster)
+	if err != nil {
+		t.Errorf("get HumioCluster: (%v)", err)
+	}
+}
