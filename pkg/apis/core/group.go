42c258fedca75d71ef368d1e3beb477fc40b3867
Jestin Woods <jestin@humio.com>
2020-02-25 10:49:03 -0800

    Add api

---
+++ pkg/apis/core/group.go
@@ -0,0 +1,6 @@
+// Package core contains core API versions.
+//
+// This file ensures Go source parsers acknowledge the core package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package core
