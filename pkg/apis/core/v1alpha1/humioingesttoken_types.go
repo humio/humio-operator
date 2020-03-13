0560f4c092bbdca103f4d33ac69ee19850fa732f
Jestin Woods <jestin@humio.com>
2020-03-13 16:19:39 -0700

    Add missed attributes

--- pkg/apis/core/v1alpha1/humioingesttoken_types.go
+++ pkg/apis/core/v1alpha1/humioingesttoken_types.go
@@ -4,21 +4,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // HumioIngestTokenSpec defines the desired state of HumioIngestToken
 type HumioIngestTokenSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Name       string `json:"name,omitempty"`
+	Parser     string `json:"parser,omitempty"`
+	Repository string `json:"repository,omitempty"`
 }
 
 // HumioIngestTokenStatus defines the observed state of HumioIngestToken
 type HumioIngestTokenStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// TODO?
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
