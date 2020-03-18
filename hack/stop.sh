1015f24d547e60cc571e597fc6339293f28b8f4d
Mike Rostermund <mike@humio.com>
2020-03-18 19:54:08 +0100

    Adjust for recent versions of kind

--- hack/stop.sh
+++ hack/stop.sh
@@ -2,17 +2,16 @@
 
 set -x
 
-# Ensure we use the correct working directory and KUBECONFIG:
+# Ensure we use the correct working directory:
 cd ~/go/src/github.com/humio/humio-operator
-export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
 # Clean up old stuff
-kubectl delete -f deploy/operator.yaml
-kubectl delete -f deploy/role_binding.yaml
-kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/role.yaml
+kubectl --context kind-kind delete -f deploy/operator.yaml
+kubectl --context kind-kind delete -f deploy/role_binding.yaml
+kubectl --context kind-kind delete -f deploy/service_account.yaml
+kubectl --context kind-kind delete -f deploy/role.yaml
 
-kubectl delete humiocluster humiocluster-sample
-helm template ~/git/cp-helm-charts --name humio --namespace=default --set cp-zookeeper.servers=1 --set cp-kafka.brokers=1 --set cp-schema-registry.enabled=false --set cp-kafka-rest.enabled=false --set cp-kafka-connect.enabled=false --set cp-ksql-server.enabled=false --set cp-control-center.enabled=false | kubectl delete -f -
-kubectl get pvc | grep -v ^NAME | cut -f1 -d' ' | xargs -I{} kubectl delete pvc {}
-kind delete cluster
+kubectl --context kind-kind ddelete humiocluster humiocluster-sample
+helm template humio ~/git/humio-cp-helm-charts --namespace=default --set cp-zookeeper.servers=1 --set cp-kafka.brokers=1 --set cp-schema-registry.enabled=false --set cp-kafka-rest.enabled=false --set cp-kafka-connect.enabled=false --set cp-ksql-server.enabled=false --set cp-control-center.enabled=false | kubectl --context kind-kind delete -f -
+kubectl --context kind-kind get pvc | grep -v ^NAME | cut -f1 -d' ' | xargs -I{} kubectl --context kind-kind delete pvc {}
+kind delete cluster --name kind
