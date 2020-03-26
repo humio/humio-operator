ef982ec877fe5f03c36700d7a17a42e3967ed602
Jestin Woods <jestin@humio.com>
2020-03-26 12:17:07 -0700

    Fix cluster state issues and pod creation

--- pkg/controller/humiocluster/pods.go
+++ pkg/controller/humiocluster/pods.go
@@ -130,9 +130,7 @@ func labelsForPod(clusterName string, nodeID int) map[string]string {
 
 func generatePodSuffix() string {
 	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789")
+	chars := []rune("abcdefghijklmnopqrstuvwxyz")
 	length := 6
 	var b strings.Builder
 	for i := 0; i < length; i++ {
