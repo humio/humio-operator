46dc35508ecf200f083d53287756189218edc73d
Mike Rostermund <mike@humio.com>
2020-10-19 10:43:49 +0200

    Use stable zone label for tests

--- hack/start-crc-cluster.sh
+++ hack/start-crc-cluster.sh
@@ -10,4 +10,4 @@ crc start --pull-secret-file=.crc-pull-secret.txt --memory 20480 --cpus 6
 eval $(crc oc-env)
 eval $(crc console --credentials | grep "To login as an admin, run" | cut -f2 -d"'")
 
-$kubectl label node --all failure-domain.beta.kubernetes.io/zone=az1
+$kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
