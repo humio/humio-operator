9d0f3e670ef1071bef027145da6df20f6e4b2369
Mike Rostermund <mike@humio.com>
2020-10-19 10:43:49 +0200

    Use stable zone label for tests

--- hack/start-kind-cluster.sh
+++ hack/start-kind-cluster.sh
@@ -10,4 +10,4 @@ kind get kubeconfig > $tmp_kubeconfig
 docker exec kind-control-plane sh -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'
 docker exec kind-control-plane sh -c 'echo options ndots:0 >> /etc/resolv.conf'
 
-$kubectl label node --overwrite --all failure-domain.beta.kubernetes.io/zone=az1
+$kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
