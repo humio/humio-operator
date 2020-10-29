e3f0e8a4dbeb62c80766ed493403b937bc7c87fa
Simon Stender Boisen <simon@humio.com>
2020-10-29 15:23:50 +0100

    Extract humio images and tags from go source
    This means we don't have to change new image tags multiple places and also makes e2e tests scripts easier to work with downstream

--- hack/run-e2e-tests-kind.sh
+++ hack/run-e2e-tests-kind.sh
@@ -8,13 +8,17 @@ declare -r envtest_assets_dir=${ENVTEST_ASSETS_DIR:-/tmp/envtest}
 declare -r ginkgo=$(go env GOPATH)/bin/ginkgo
 declare -r proxy_method=${PROXY_METHOD:-inject-tcp}
 
-# Preload default humio-core container version
-docker pull humio/humio-core:1.16.1
-kind load docker-image --name kind humio/humio-core:1.16.1
+# Extract humio images and tags from go source
+DEFAULT_IMAGE=$(grep '^\s*image' controllers/humiocluster_defaults.go | cut -d '"' -f 2)
+PRE_UPDATE_IMAGE=$(grep '^\s*toCreate\.Spec\.Image' controllers/humiocluster_controller_test.go | cut -d '"' -f 2)
 
-# Preload humio-core used by e2e tests
-docker pull humio/humio-core:1.14.5
-kind load docker-image --name kind humio/humio-core:1.14.5
+# Preload default image used by tests
+docker pull $DEFAULT_IMAGE
+kind load docker-image --name kind $DEFAULT_IMAGE
+
+# Preload image used by e2e update tests
+docker pull $PRE_UPDATE_IMAGE
+kind load docker-image --name kind $PRE_UPDATE_IMAGE
 
 $kubectl apply -k config/crd/
 $kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
