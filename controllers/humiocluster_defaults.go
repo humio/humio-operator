5551fe4d55d4d09ab7b4225995e17f07460332a4
Mike Rostermund <mike@humio.com>
2020-12-22 11:46:02 +0100

    Bump default to humio/humio-core:1.18.1

--- controllers/humiocluster_defaults.go
+++ controllers/humiocluster_defaults.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	image                        = "humio/humio-core:1.18.0"
+	image                        = "humio/humio-core:1.18.1"
 	helperImage                  = "humio/humio-operator-helper:0.2.0"
 	targetReplicationFactor      = 2
 	storagePartitionsCount       = 24
