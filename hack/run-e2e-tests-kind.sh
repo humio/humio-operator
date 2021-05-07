62ed31faa24e4adbcdcfaf051a93edc8167fd4e4
Mike Rostermund <mike@humio.com>
2021-05-07 13:57:55 +0200

    Switch to Telepresence 2

--- hack/run-e2e-tests-kind.sh
+++ hack/run-e2e-tests-kind.sh
@@ -6,7 +6,8 @@ declare -r tmp_kubeconfig=/tmp/kubeconfig
 declare -r kubectl="kubectl --kubeconfig $tmp_kubeconfig"
 declare -r envtest_assets_dir=${ENVTEST_ASSETS_DIR:-/tmp/envtest}
 declare -r ginkgo=$(go env GOPATH)/bin/ginkgo
-declare -r proxy_method=${PROXY_METHOD:-inject-tcp}
+
+export PATH=$BIN_DIR:$PATH
 
 # Extract humio images and tags from go source
 DEFAULT_IMAGE=$(grep '^\s*image' controllers/humiocluster_defaults.go | cut -d '"' -f 2)
@@ -26,4 +27,6 @@ $kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
 # TODO: add -p to automatically detect optimal number of test nodes, OR, -nodes=n to set parallelism, and add -stream to output logs from tests running in parallel.
 # We skip the helpers package as those tests assumes the environment variable USE_CERT_MANAGER is not set.
 # Documentation for Go support states that inject-tcp method will not work. https://www.telepresence.io/howto/golang
-USE_CERTMANAGER=true TEST_USE_EXISTING_CLUSTER=true telepresence --method $proxy_method --run $ginkgo -timeout 60m -skipPackage helpers -v ./... -covermode=count -coverprofile cover.out -progress
+telepresence connect
+USE_CERTMANAGER=true TEST_USE_EXISTING_CLUSTER=true $ginkgo -timeout 60m -skipPackage helpers -v ./... -covermode=count -coverprofile cover.out -progress
+telepresence uninstall --everything
