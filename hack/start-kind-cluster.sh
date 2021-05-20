55b3d172842aaa279b2fdaee833175bbdc7b79e0
Mike Rostermund <mike@humio.com>
2021-05-20 12:15:03 +0200

    Bump kind to latest version
    Stick with same k8s minor versions for now

--- hack/start-kind-cluster.sh
+++ hack/start-kind-cluster.sh
@@ -5,7 +5,7 @@ set -x
 declare -r tmp_kubeconfig=/tmp/kubeconfig
 declare -r kubectl="kubectl --kubeconfig $tmp_kubeconfig"
 
-kind create cluster --name kind --image kindest/node:v1.17.11
+kind create cluster --name kind --image kindest/node:v1.17.17@sha256:c581fbf67f720f70aaabc74b44c2332cc753df262b6c0bca5d26338492470c17
 kind get kubeconfig > $tmp_kubeconfig
 docker exec kind-control-plane sh -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'
 docker exec kind-control-plane sh -c 'echo options ndots:0 >> /etc/resolv.conf'
