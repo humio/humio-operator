8624753df327cdce525aa69734a71da9d8b1a87c
Mike Rostermund <mike@humio.com>
2021-06-09 16:37:04 +0200

    Use RFC3339 format for operator log timestamps

--- controllers/humioaction_controller.go
+++ controllers/humioaction_controller.go
@@ -26,7 +26,6 @@ import (
 	"github.com/go-logr/zapr"
 	humioapi "github.com/humio/cli/api"
 	"github.com/humio/humio-operator/pkg/helpers"
-	uberzap "go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
@@ -48,7 +47,7 @@ type HumioActionReconciler struct {
 //+kubebuilder:rbac:groups=core.humio.com,resources=humioactions/finalizers,verbs=update
 
 func (r *HumioActionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	zapLog, _ := uberzap.NewProduction(uberzap.AddCaller(), uberzap.AddCallerSkip(1))
+	zapLog, _ := helpers.NewLogger()
 	defer zapLog.Sync()
 	r.Log = zapr.NewLogger(zapLog).WithValues("Request.Namespace", req.Namespace, "Request.Name", req.Name, "Request.Type", helpers.GetTypeName(r))
 	r.Log.Info("Reconciling HumioAction")
--- controllers/humiorepository_controller.go
+++ controllers/humiorepository_controller.go
@@ -22,7 +22,6 @@ import (
 	"github.com/go-logr/zapr"
 	humioapi "github.com/humio/cli/api"
 	"github.com/humio/humio-operator/pkg/helpers"
-	uberzap "go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -48,7 +47,7 @@ type HumioRepositoryReconciler struct {
 //+kubebuilder:rbac:groups=core.humio.com,resources=humiorepositories/finalizers,verbs=update
 
 func (r *HumioRepositoryReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	zapLog, _ := uberzap.NewProduction(uberzap.AddCaller(), uberzap.AddCallerSkip(1))
+	zapLog, _ := helpers.NewLogger()
 	defer zapLog.Sync()
 	r.Log = zapr.NewLogger(zapLog).WithValues("Request.Namespace", req.Namespace, "Request.Name", req.Name, "Request.Type", helpers.GetTypeName(r))
 	r.Log.Info("Reconciling HumioRepository")
