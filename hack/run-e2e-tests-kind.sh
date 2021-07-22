2e5379813412567789a7672472594e7e684fffc2
Mike Rostermund <mike@humio.com>
2021-07-22 17:14:01 +0200

    Stop using Telepresence to run E2E tests
    Instead we run a pod inside the `kind` cluster with the source code and
    tools to compile and run the tests inside the cluster itself.
    
    This unfortunately means the tests using `crc` for testing will not
    work. We use `kind load docker-image` to load the container where the
    tests will be performed within and `crc` doesn't have a similar command.
    This means that the `crc` scripts will likely need to either push the
    containers to a remote registry, or potentially "docker push" the image
    directly to a registry running inside the `crc` cluster.

--- hack/run-e2e-tests-kind.sh
+++ hack/run-e2e-tests-kind.sh
@@ -2,10 +2,13 @@
 
 set -x
 
-declare -r tmp_kubeconfig=/tmp/kubeconfig
-declare -r kubectl="kubectl --kubeconfig $tmp_kubeconfig"
 declare -r envtest_assets_dir=${ENVTEST_ASSETS_DIR:-/tmp/envtest}
 declare -r ginkgo=$(go env GOPATH)/bin/ginkgo
+
+if ! kubectl get daemonset -n kube-system kindnet ; then
+  echo "Cluster unavailable or not using a kind cluster. Only kind clusters are supported!"
+  exit 1
+fi
 
 if [[ -z "${HUMIO_E2E_LICENSE}" ]]; then
   echo "Environment variable HUMIO_E2E_LICENSE not set. Aborting."
@@ -14,32 +17,8 @@ fi
 
 export PATH=$BIN_DIR:$PATH
 
-trap cleanup exit
-
-cleanup() {
-  telepresence uninstall --kubeconfig $tmp_kubeconfig --everything
-}
-
-# Extract humio images and tags from go source
-DEFAULT_IMAGE=$(grep '^\s*image' controllers/humiocluster_defaults.go | cut -d '"' -f 2)
-PRE_UPDATE_IMAGE=$(grep '^\s*toCreate\.Spec\.Image' controllers/humiocluster_controller_test.go | cut -d '"' -f 2)
-
-# Preload default image used by tests
-docker pull $DEFAULT_IMAGE
-kind load docker-image --name kind $DEFAULT_IMAGE
-
-# Preload image used by e2e update tests
-docker pull $PRE_UPDATE_IMAGE
-kind load docker-image --name kind $PRE_UPDATE_IMAGE
-
-$kubectl apply -k config/crd/
-$kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
-
-# TODO: add -p to automatically detect optimal number of test nodes, OR, -nodes=n to set parallelism, and add -stream to output logs from tests running in parallel.
-# We skip the helpers package as those tests assumes the environment variable USE_CERT_MANAGER is not set.
-# Documentation for Go support states that inject-tcp method will not work. https://www.telepresence.io/howto/golang
-echo "NOTE: Running 'telepresence connect' needs root access so it will prompt for the password of the user account to set up rules with iptables (or similar)"
-telepresence connect --kubeconfig $tmp_kubeconfig
+kubectl apply -k config/crd/
+kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
 
 iterations=0
 while ! curl -k https://kubernetes.default
@@ -52,4 +31,8 @@ do
   sleep 2
 done
 
-KUBECONFIG=$tmp_kubeconfig USE_CERTMANAGER=true TEST_USE_EXISTING_CLUSTER=true $ginkgo -timeout 90m -skipPackage helpers -v ./... -covermode=count -coverprofile cover.out -progress
+make ginkgo
+
+# TODO: add -p to automatically detect optimal number of test nodes, OR, -nodes=n to set parallelism, and add -stream to output logs from tests running in parallel.
+# We skip the helpers package as those tests assumes the environment variable USE_CERT_MANAGER is not set.
+USE_CERTMANAGER=true TEST_USE_EXISTING_CLUSTER=true $ginkgo -timeout 90m -skipPackage helpers -v ./... -covermode=count -coverprofile cover.out -progress
