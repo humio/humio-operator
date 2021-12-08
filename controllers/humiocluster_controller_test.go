59d1c63798243d86e98c03c348455f624ee05e4c
Jestin Woods <jestin.woods@crowdstrike.com>
2021-12-08 09:50:21 -0800

    Bump default humio version to 1.32.3

--- controllers/humiocluster_controller_test.go
+++ controllers/humiocluster_controller_test.go
@@ -177,7 +177,7 @@ var _ = Describe("HumioCluster Controller", func() {
 				Namespace: testProcessID,
 			}
 			toCreate := constructBasicSingleNodeHumioCluster(key, true)
-			toCreate.Spec.Image = "humio/humio-core:1.28.0"
+			toCreate.Spec.Image = "humio/humio-core:1.30.0"
 			toCreate.Spec.NodeCount = helpers.IntPtr(2)
 
 			usingClusterBy(key.Name, "Creating the cluster successfully")
@@ -249,7 +249,7 @@ var _ = Describe("HumioCluster Controller", func() {
 				Name:      "humiocluster-update-image-np",
 				Namespace: testProcessID,
 			}
-			originalImage := "humio/humio-core:1.28.0"
+			originalImage := "humio/humio-core:1.30.0"
 			toCreate := constructBasicMultiNodePoolHumioCluster(key, true, 1)
 			toCreate.Spec.Image = originalImage
 			toCreate.Spec.NodeCount = helpers.IntPtr(1)
@@ -415,7 +415,7 @@ var _ = Describe("HumioCluster Controller", func() {
 				Namespace: testProcessID,
 			}
 			toCreate := constructBasicSingleNodeHumioCluster(key, true)
-			toCreate.Spec.Image = "humio/humio-core:1.28.0"
+			toCreate.Spec.Image = "humio/humio-core:1.30.0"
 			toCreate.Spec.NodeCount = helpers.IntPtr(2)
 
 			usingClusterBy(key.Name, "Creating the cluster successfully")
--- controllers/humiocluster_defaults.go
+++ controllers/humiocluster_defaults.go
@@ -32,7 +32,7 @@ import (
 )
 
 const (
-	image                        = "humio/humio-core:1.30.1"
+	image                        = "humio/humio-core:1.32.3"
 	helperImage                  = "humio/humio-operator-helper:0.5.0"
 	targetReplicationFactor      = 2
 	storagePartitionsCount       = 24
