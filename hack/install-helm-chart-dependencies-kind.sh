85a16fdfede4c7601df0de3b5af3ae775a6d2bdf
Jestin Woods <jestin.woods@crowdstrike.com>
2022-02-16 16:19:54 -0800

    More docker auth

--- hack/install-helm-chart-dependencies-kind.sh
+++ hack/install-helm-chart-dependencies-kind.sh
@@ -16,6 +16,10 @@ if ! kubectl get daemonset -n kube-system kindnet ; then
   exit 1
 fi
 
+if [[ $DOCKER_USERNAME != "" ]] && [[ $DOCKER_PASSWORD != "" ]]; then
+  kubectl create secret docker-registry regcred --docker-server="https://index.docker.io/v1/" --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --namespace default
+fi
+
 if [[ $humio_hostname != "none" ]] && [[ $humio_ingest_token != "none" ]]; then
 
   export E2E_FILTER_TAG=$(cat <<EOF
@@ -30,7 +34,7 @@ EOF
 )
 
   helm repo add shipper https://humio.github.io/humio-helm-charts
-  helm install log-shipper shipper/humio-helm-charts --namespace=default \
+  helm_install_command="helm install log-shipper shipper/humio-helm-charts --namespace=default \
   --set humio-fluentbit.enabled=true \
   --set humio-fluentbit.es.port=443 \
   --set humio-fluentbit.es.tls=true \
@@ -45,21 +49,45 @@ EOF
   --set humio-metrics.es.autodiscovery=false \
   --set humio-metrics.publish.enabled=false \
   --set humio-metrics.humioHostname=$humio_hostname \
-  --set humio-metrics.token=$humio_ingest_token
+  --set humio-metrics.token=$humio_ingest_token"
+
+  if [[ $DOCKER_USERNAME != "" ]] && [[ $DOCKER_PASSWORD != "" ]]; then
+    helm_install_command="${helm_install_command} \
+    --set humio-fluentbit.imagePullSecrets[0].name=regcred \
+    --set humio-metrics.imagePullSecrets[0].name=regcred"
+  fi
+  $helm_install_command
 fi
 
 kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install cert-manager jetstack/cert-manager --namespace cert-manager \
+helm_install_command="helm install cert-manager jetstack/cert-manager --namespace cert-manager \
 --version v1.5.3 \
---set installCRDs=true
+--set installCRDs=true"
+
+if [[ $DOCKER_USERNAME != "" ]] && [[ $DOCKER_PASSWORD != "" ]]; then
+  kubectl create secret docker-registry regcred --docker-server="https://index.docker.io/v1/" --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --namespace cert-manager
+  helm_install_command="${helm_install_command} \
+  --set global.imagePullSecrets[0].name=regcred"
+fi
+
+$helm_install_command
 
 helm repo add humio https://humio.github.io/cp-helm-charts
-helm install humio humio/cp-helm-charts --namespace=default \
+helm_install_command="helm install humio humio/cp-helm-charts --namespace=default \
 --set cp-zookeeper.servers=1 --set cp-kafka.brokers=1 --set cp-schema-registry.enabled=false \
 --set cp-kafka-rest.enabled=false --set cp-kafka-connect.enabled=false \
---set cp-ksql-server.enabled=false --set cp-control-center.enabled=false
+--set cp-ksql-server.enabled=false --set cp-control-center.enabled=false"
+
+if [[ $DOCKER_USERNAME != "" ]] && [[ $DOCKER_PASSWORD != "" ]]; then
+  helm_install_command="${helm_install_command} \
+  --set cp-zookeeper.imagePullSecrets[0].name=regcred \"
+  --set cp-kafka-rest.imagePullSecrets[0].name=regcred \"
+  --set cp-ksql-server.imagePullSecrets[0].name=regcred"
+fi
+
+$helm_install_command
 
 while [[ $(kubectl get pods humio-cp-zookeeper-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]
 do
