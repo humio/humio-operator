0113741c33a42875a5a15cbf0fd091012a7e6090
Mike Rostermund <mike@humio.com>
2022-02-22 08:05:19 +0100

    Fix E2E exit code is set correctly

--- hack/run-e2e-tests-using-kubectl-kind.sh
+++ hack/run-e2e-tests-using-kubectl-kind.sh
@@ -15,6 +15,9 @@ kubectl patch clusterrolebinding cluster-admin --type='json' -p='[{"op": "add", 
 kubectl run test-pod --env="HUMIO_E2E_LICENSE=$HUMIO_E2E_LICENSE" --env="E2E_LOGS_HUMIO_HOSTNAME=$E2E_LOGS_HUMIO_HOSTNAME" --env="E2E_LOGS_HUMIO_INGEST_TOKEN=$E2E_LOGS_HUMIO_INGEST_TOKEN" --env="E2E_RUN_ID=$E2E_RUN_ID" --env="GINKGO_NODES=$GINKGO_NODES" --env="DOCKER_USERNAME=$DOCKER_USERNAME" --env="DOCKER_PASSWORD=$DOCKER_PASSWORD" --restart=Never --image=testcontainer --image-pull-policy=Never -- sleep 86400
 while [[ $(kubectl get pods test-pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" ; kubectl describe pod test-pod ; sleep 1 ; done
 kubectl exec test-pod -- hack/run-e2e-tests-kind.sh
+TEST_EXIT_CODE=$?
 
 end=$(date +%s)
 echo "Running e2e tests with kubectl exec took $((end-start)) seconds"
+
+exit "$TEST_EXIT_CODE"
