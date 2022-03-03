7fa4bf1efaaed9097ea96121a7a2c80940286adf
Mike Rostermund <mike@humio.com>
2022-03-03 13:25:43 +0100

    Refactor Helm chart resources
    The main change here is to better generate resource names for the
    resources created by installing the Helm chart.
    
    This also removes the unnecessary single quote characters around values
    in the YAML files, which makes syntax highlighting slightly better for
    some editors.
    
    Ideally we'd also change it so that the resource labels would use the
    names that can now be overridden, but I'm leaving that out for now.
    This also removes the `OPERATOR_NAME` environment variable from the
    Deployment resource as we don't use it at all, but perhaps we should
    later consider adding it back again and populating it e.g. `fullname`
    such that any resources created by this operator installation would
    create and only touch resources that it itself installed. This may make
    it easier for us to better support running multiple operators on the
    same Kubernetes cluster.
    
    Fixes: https://github.com/humio/humio-operator/issues/574

--- charts/humio-operator/templates/_helpers.tpl
+++ charts/humio-operator/templates/_helpers.tpl
@@ -1,6 +1,29 @@
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "humio.chart" -}}
-{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
+{{- define "operator.chart" -}}
+  {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Name of the operator which is used to construct operator.fullname.
+*/}}
+{{- define "operator.name" -}}
+  {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Construct fullname used to name resources created by the chart.
+*/}}
+{{- define "operator.fullname" -}}
+  {{- if .Values.fullnameOverride -}}
+    {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+  {{- else -}}
+    {{- $name := default .Chart.Name .Values.nameOverride -}}
+    {{- if contains $name .Release.Name -}}
+      {{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+    {{- else -}}
+      {{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+    {{- end -}}
+  {{- end -}}
 {{- end -}}
--- hack/gen-crds.sh
+++ hack/gen-crds.sh
@@ -13,17 +13,17 @@ for c in $(find config/crd/bases/ -iname '*.yaml' | sort); do
 
   # Update base CRD's in-place with static values
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sed -i "/^spec:/i \  labels:\n    app: 'humio-operator'\n    app.kubernetes.io/name: 'humio-operator'\n    app.kubernetes.io/instance: 'humio-operator'\n    app.kubernetes.io/managed-by: 'Helm'\n    helm.sh/chart: 'humio-operator-$RELEASE_VERSION'" $c
+    sed -i "/^spec:/i \  labels:\n    app: humio-operator\n    app.kubernetes.io/name: humio-operator\n    app.kubernetes.io/instance: humio-operator\n    app.kubernetes.io/managed-by: Helm\n    helm.sh/chart: humio-operator-$RELEASE_VERSION" $c
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     if [[ $(which gsed) ]]; then
-      gsed -i "/^spec:/i \  labels:\n    app: 'humio-operator'\n    app.kubernetes.io/name: 'humio-operator'\n    app.kubernetes.io/instance: 'humio-operator'\n    app.kubernetes.io/managed-by: 'Helm'\n    helm.sh/chart: 'humio-operator-$RELEASE_VERSION'" $c
+      gsed -i "/^spec:/i \  labels:\n    app: humio-operator\n    app.kubernetes.io/name: humio-operator\n    app.kubernetes.io/instance: humio-operator\n    app.kubernetes.io/managed-by: Helm\n    helm.sh/chart: humio-operator-$RELEASE_VERSION" $c
     else
       sed -i '' -E '/^spec:/i\ '$'\n''\  labels:'$'\n' $c
-      sed -i '' -E '/^spec:/i\ '$'\n''\    app: '"'humio-operator'"$'\n' $c
-      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/name: '"'humio-operator'"$'\n' $c
-      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/instance: '"'humio-operator'"$'\n' $c
-      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/managed-by: '"'Helm'"$'\n' $c
-      sed -i '' -E '/^spec:/i\ '$'\n''\    helm.sh/chart: '"'humio-operator-$RELEASE_VERSION'"$'\n' $c
+      sed -i '' -E '/^spec:/i\ '$'\n''\    app: '"humio-operator"$'\n' $c
+      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/name: '"humio-operator"$'\n' $c
+      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/instance: '"humio-operator"$'\n' $c
+      sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/managed-by: '"Helm"$'\n' $c
+      sed -i '' -E '/^spec:/i\ '$'\n''\    helm.sh/chart: '"humio-operator-$RELEASE_VERSION"$'\n' $c
     fi
   else
     echo "$OSTYPE not supported"
@@ -34,17 +34,17 @@ echo "{{- end }}" >> charts/humio-operator/templates/crds.yaml
 
 # Update helm chart CRD's with additional chart install values.
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sed -i "/^spec:/i \  labels:\n    app: '{{ .Chart.Name }}'\n    app.kubernetes.io/name: '{{ .Chart.Name }}'\n    app.kubernetes.io/instance: '{{ .Release.Name }}'\n    app.kubernetes.io/managed-by: '{{ .Release.Service }}'\n    helm.sh/chart: '{{ template \"humio.chart\" . }}'" charts/humio-operator/templates/crds.yaml
+	sed -i "/^spec:/i \  labels:\n    app: {{ .Chart.Name }}\n    app.kubernetes.io/name: {{ .Chart.Name }}\n    app.kubernetes.io/instance: {{ .Release.Name }}\n    app.kubernetes.io/managed-by: {{ .Release.Service }}\n    helm.sh/chart: {{ template \"operator.chart\" . }}" charts/humio-operator/templates/crds.yaml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   if [[ $(which gsed) ]]; then
-	  gsed -i "/^spec:/i \  labels:\n    app: '{{ .Chart.Name }}'\n    app.kubernetes.io/name: '{{ .Chart.Name }}'\n    app.kubernetes.io/instance: '{{ .Release.Name }}'\n    app.kubernetes.io/managed-by: '{{ .Release.Service }}'\n    helm.sh/chart: '{{ template \"humio.chart\" . }}'" charts/humio-operator/templates/crds.yaml
+	  gsed -i "/^spec:/i \  labels:\n    app: {{ .Chart.Name }}\n    app.kubernetes.io/name: {{ .Chart.Name }}\n    app.kubernetes.io/instance: {{ .Release.Name }}\n    app.kubernetes.io/managed-by: {{ .Release.Service }}\n    helm.sh/chart: {{ template \"operator.chart\" . }}" charts/humio-operator/templates/crds.yaml
   else
     sed -i '' -E '/^spec:/i\ '$'\n''\  labels:'$'\n' charts/humio-operator/templates/crds.yaml
-    sed -i '' -E '/^spec:/i\ '$'\n''\    app: '"'{{ .Chart.Name }}'"$'\n' charts/humio-operator/templates/crds.yaml
-    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/name: '"'{{ .Chart.Name }}'"$'\n' charts/humio-operator/templates/crds.yaml
-    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/instance: '"'{{ .Release.Name }}'"$'\n' charts/humio-operator/templates/crds.yaml
-    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/managed-by: '"'{{ .Release.Service }}'"$'\n' charts/humio-operator/templates/crds.yaml
-    sed -i '' -E '/^spec:/i\ '$'\n''\    helm.sh/chart: '"'{{ template \"humio.chart\" . }}'"$'\n' charts/humio-operator/templates/crds.yaml
+    sed -i '' -E '/^spec:/i\ '$'\n''\    app: '"{{ .Chart.Name }}"$'\n' charts/humio-operator/templates/crds.yaml
+    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/name: '"{{ .Chart.Name }}"$'\n' charts/humio-operator/templates/crds.yaml
+    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/instance: '"{{ .Release.Name }}"$'\n' charts/humio-operator/templates/crds.yaml
+    sed -i '' -E '/^spec:/i\ '$'\n''\    app.kubernetes.io/managed-by: '"{{ .Release.Service }}"$'\n' charts/humio-operator/templates/crds.yaml
+    sed -i '' -E '/^spec:/i\ '$'\n''\    helm.sh/chart: '"{{ template \"operator.chart\" . }}"$'\n' charts/humio-operator/templates/crds.yaml
   fi
 else
   echo "$OSTYPE not supported"
