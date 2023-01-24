f1466e07ce19b6fdb9e44addd54b97f1b26d1def
Mike Rostermund <mike.rostermund@crowdstrike.com>
2023-01-24 15:26:29 +0100

    Skip jmx exporter for zk/kafka during e2e tests

--- hack/install-helm-chart-dependencies-crc.sh
+++ hack/install-helm-chart-dependencies-crc.sh
@@ -17,9 +17,13 @@ helm install --kubeconfig=$tmp_kubeconfig cert-manager jetstack/cert-manager --n
 
 helm repo add humio https://humio.github.io/cp-helm-charts
 helm install --kubeconfig=$tmp_kubeconfig humio humio/cp-helm-charts --namespace=default \
---set cp-zookeeper.servers=1 --set cp-kafka.brokers=1 --set cp-schema-registry.enabled=false \
---set cp-kafka-rest.enabled=false --set cp-kafka-connect.enabled=false \
---set cp-ksql-server.enabled=false --set cp-control-center.enabled=false
+--set cp-zookeeper.servers=1 --set cp-zookeeper.prometheus.jmx.enabled=false \
+--set cp-kafka.brokers=1 --set cp-kafka.prometheus.jmx.enabled=false \
+--set cp-schema-registry.enabled=false \
+--set cp-kafka-rest.enabled=false \
+--set cp-kafka-connect.enabled=false \
+--set cp-ksql-server.enabled=false \
+--set cp-control-center.enabled=false
 
 while [[ $(oc --kubeconfig=$tmp_kubeconfig get pods humio-cp-zookeeper-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]
 do
