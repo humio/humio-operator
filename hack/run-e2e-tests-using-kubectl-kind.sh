e422ae45d1d079cbf0cdebf4118ec99654325224
Jestin Woods <jestin.woods@crowdstrike.com>
2023-11-16 13:02:51 -0800

    Apply CRDs when running e2e tests using kubectl

--- hack/run-e2e-tests-using-kubectl-kind.sh
+++ hack/run-e2e-tests-using-kubectl-kind.sh
@@ -11,6 +11,9 @@ if ! kubectl get daemonset -n kube-system kindnet ; then
   exit 1
 fi
 
+kubectl create -k config/crd/
+kubectl label node --overwrite --all topology.kubernetes.io/zone=az1
+
 kubectl patch clusterrolebinding cluster-admin --type='json' -p='[{"op": "add", "path": "/subjects/1", "value": {"kind": "ServiceAccount", "name": "default", "namespace": "default" } }]'
 kubectl run -n default test-pod --env="HUMIO_E2E_LICENSE=$HUMIO_E2E_LICENSE" --env="E2E_LOGS_HUMIO_HOSTNAME=$E2E_LOGS_HUMIO_HOSTNAME" --env="E2E_LOGS_HUMIO_INGEST_TOKEN=$E2E_LOGS_HUMIO_INGEST_TOKEN" --env="E2E_RUN_ID=$E2E_RUN_ID" --env="GINKGO_NODES=$GINKGO_NODES" --env="DOCKER_USERNAME=$DOCKER_USERNAME" --env="DOCKER_PASSWORD=$DOCKER_PASSWORD" --restart=Never --image=testcontainer --image-pull-policy=Never -- sleep 86400
 while [[ $(kubectl get -n default pods test-pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" ; kubectl describe -n default pod test-pod ; sleep 1 ; done
