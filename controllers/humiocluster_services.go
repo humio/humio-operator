7ed1d006605dd779e91336739e77d2db7b8c1b7c
Jestin Woods <jestin.woods@crowdstrike.com>
2024-03-15 10:43:05 -0700

    Fix bug where orphaned node pool statuses block cluster upgrade operations

--- controllers/humiocluster_services.go
+++ controllers/humiocluster_services.go
@@ -19,9 +19,8 @@ package controllers
 import (
 	"fmt"
 
-	"github.com/humio/humio-operator/pkg/helpers"
-
 	humiov1alpha1 "github.com/humio/humio-operator/api/v1alpha1"
+	"github.com/humio/humio-operator/pkg/helpers"
 	"github.com/humio/humio-operator/pkg/kubernetes"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
