5aa54eb7cb767d9c7ab02991e3bc3bd3e08640ab
Jestin Woods <jestin.woods@crowdstrike.com>
2024-07-10 11:04:22 -0700

    cleanup

--- pkg/humio/filteralert_transform.go
+++ pkg/humio/filteralert_transform.go
@@ -3,7 +3,6 @@ package humio
 import (
 	humioapi "github.com/humio/cli/api"
 	humiov1alpha1 "github.com/humio/humio-operator/api/v1alpha1"
-	"github.com/humio/humio-operator/pkg/helpers"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,18 +13,15 @@ const (
 
 func FilterAlertTransform(hfa *humiov1alpha1.HumioFilterAlert) (*humioapi.FilterAlert, error) {
 	filterAlert := &humioapi.FilterAlert{
-		Name:               hfa.Spec.Name,
-		QueryString:        hfa.Spec.QueryString,
-		Description:        hfa.Spec.Description,
-		ThrottleField:      hfa.Spec.ThrottleField,
-		Enabled:            hfa.Spec.Enabled,
-		ActionNames:        hfa.Spec.Actions,
-		Labels:             hfa.Spec.Labels,
-		QueryOwnershipType: QueryOwnershipTypeDefault,
-	}
-
-	if hfa.Spec.ThrottleTimeSeconds != nil {
-		filterAlert.ThrottleTimeSeconds = *hfa.Spec.ThrottleTimeSeconds
+		Name:                hfa.Spec.Name,
+		QueryString:         hfa.Spec.QueryString,
+		Description:         hfa.Spec.Description,
+		ThrottleTimeSeconds: hfa.Spec.ThrottleTimeSeconds,
+		ThrottleField:       hfa.Spec.ThrottleField,
+		Enabled:             hfa.Spec.Enabled,
+		ActionNames:         hfa.Spec.Actions,
+		Labels:              hfa.Spec.Labels,
+		QueryOwnershipType:  QueryOwnershipTypeDefault,
 	}
 
 	if _, ok := hfa.ObjectMeta.Annotations[FilterAlertIdentifierAnnotation]; ok {
@@ -37,17 +33,14 @@ func FilterAlertTransform(hfa *humiov1alpha1.HumioFilterAlert) (*humioapi.Filter
 
 func FilterAlertHydrate(hfa *humiov1alpha1.HumioFilterAlert, alert *humioapi.FilterAlert) error {
 	hfa.Spec = humiov1alpha1.HumioFilterAlertSpec{
-		Name:          alert.Name,
-		QueryString:   alert.QueryString,
-		Description:   alert.Description,
-		ThrottleField: alert.ThrottleField,
-		Enabled:       alert.Enabled,
-		Actions:       alert.ActionNames,
-		Labels:        alert.Labels,
-	}
-
-	if alert.ThrottleTimeSeconds != 0 {
-		hfa.Spec.ThrottleTimeSeconds = helpers.IntPtr(alert.ThrottleTimeSeconds)
+		Name:                alert.Name,
+		QueryString:         alert.QueryString,
+		Description:         alert.Description,
+		ThrottleTimeSeconds: alert.ThrottleTimeSeconds,
+		ThrottleField:       alert.ThrottleField,
+		Enabled:             alert.Enabled,
+		Actions:             alert.ActionNames,
+		Labels:              alert.Labels,
 	}
 
 	hfa.ObjectMeta = metav1.ObjectMeta{
