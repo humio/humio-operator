0ffcf46a6c47955a991b9b91e1a1da9c428201ab
Rafael Abdalla <rafael.abdalla@crowdstrike.com>
2024-08-07 15:22:25 +1000

    Fixed git conflict

--- controllers/suite/resources/humioresources_controller_test.go
+++ controllers/suite/resources/humioresources_controller_test.go
@@ -3115,11 +3115,7 @@ var _ = Describe("Humio Resources Controllers", func() {
 				Spec: aggregateAlertSpec,
 			}
 
-<<<<<<< HEAD
 			suite.UsingClusterBy(clusterKey.Name, "HumioAggregateAlert: Creating the aggregate alert successfully")
-=======
-			suite.UsingClusterBy(clusterKey.Name, "HumioAlert: Creating the aggregate alert successfully")
->>>>>>> dad47d2 (Created AggregateAlerts Support)
 			Expect(k8sClient.Create(ctx, toCreateAggregateAlert)).Should(Succeed())
 
 			fetchedAggregateAlert := &humiov1alpha1.HumioAggregateAlert{}
