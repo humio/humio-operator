118dfbb5dfe2b5cf5e50d3987baa67f26a758e1f
Rafael Abdalla <rafael.abdalla@crowdstrike.com>
2024-08-23 12:08:44 +1000

    Set sidecar image value

--- controllers/humiocluster_defaults.go
+++ controllers/humiocluster_defaults.go
@@ -34,6 +34,7 @@ import (
 
 const (
 	Image                        = "humio/humio-core:1.131.1"
+	SidecarImage                 = "humio/humio-core:1.131.1"
 	HelperImage                  = "humio/humio-operator-helper:8f5ef6c7e470226e77d985f36cf39be9a100afea"
 	targetReplicationFactor      = 2
 	digestPartitionsCount        = 24
