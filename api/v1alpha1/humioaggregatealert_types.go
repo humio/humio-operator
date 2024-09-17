44d0452e7e2c0365c16f06c2c3195391c5c7273f
Mike Rostermund <mike@humio.com>
2024-09-17 11:05:04 +0200

    Require non empty resource names (#854)
    * Require non empty resource names
    
    * Log out test case failure reason if present
    
    * mock: use lock when accessing map with user details

--- api/v1alpha1/humioaggregatealert_types.go
+++ api/v1alpha1/humioaggregatealert_types.go
@@ -41,14 +41,19 @@ type HumioAggregateAlertSpec struct {
 	// This conflicts with ManagedClusterName.
 	ExternalClusterName string `json:"externalClusterName,omitempty"`
 	// Name is the name of the aggregate alert inside Humio
+	//+kubebuilder:validation:MinLength=1
+	//+required
 	Name string `json:"name"`
 	// ViewName is the name of the Humio View under which the aggregate alert will be managed. This can also be a Repository
+	//+kubebuilder:validation:MinLength=1
+	//+required
 	ViewName string `json:"viewName"`
 	// QueryString defines the desired Humio query string
 	QueryString string `json:"queryString"`
 	// QueryTimestampType defines the timestamp type to use for a query
 	QueryTimestampType string `json:"queryTimestampType,omitempty"`
 	// Description is the description of the Aggregate alert
+	//+optional
 	Description string `json:"description,omitempty"`
 	// Search Interval time in seconds
 	SearchIntervalSeconds int `json:"searchIntervalSeconds,omitempty"`
--- api/v1alpha1/humiorepository_types.go
+++ api/v1alpha1/humiorepository_types.go
@@ -50,8 +50,11 @@ type HumioRepositorySpec struct {
 	// This conflicts with ManagedClusterName.
 	ExternalClusterName string `json:"externalClusterName,omitempty"`
 	// Name is the name of the repository inside Humio
-	Name string `json:"name,omitempty"`
+	//+kubebuilder:validation:MinLength=1
+	//+required
+	Name string `json:"name"`
 	// Description contains the description that will be set on the repository
+	//+optional
 	Description string `json:"description,omitempty"`
 	// Retention defines the retention settings for the repository
 	Retention HumioRetention `json:"retention,omitempty"`
