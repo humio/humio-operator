6f8be48bfa98477b9ef92025d515904b6732e120
Mike Rostermund <mike.rostermund@crowdstrike.com>
2025-03-20 08:56:26 +0100

    Adjust ginkgo executions to work with Ginkgo v2.23.1+

--- hack/run-e2e-within-kind-test-pod-dummy.sh
+++ hack/run-e2e-within-kind-test-pod-dummy.sh
@@ -5,4 +5,4 @@ set -x -o pipefail
 source hack/functions.sh
 
 # We skip the helpers package as those tests assumes the environment variable USE_CERT_MANAGER is not set.
-DUMMY_LOGSCALE_IMAGE=true ginkgo --label-filter=dummy -timeout 90m -procs=$GINKGO_NODES --no-color --skip-package helpers -v ./internal/controller/suite/... -covermode=count -coverprofile cover.out -progress | tee /proc/1/fd/1
+DUMMY_LOGSCALE_IMAGE=true ginkgo run --label-filter=dummy -timeout 90m -procs=$GINKGO_NODES --no-color --skip-package helpers -v -progress ./internal/controller/suite/... | tee /proc/1/fd/1
