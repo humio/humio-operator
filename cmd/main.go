b849f5ee1a54de414de72ca2cdba20e6c2539714
Mike Rostermund <mike.rostermund@crowdstrike.com>
2025-05-14 11:19:10 +0200

    Introduce HumioViewPermissionRole CRD

--- cmd/main.go
+++ cmd/main.go
@@ -418,4 +418,15 @@ func setupControllers(mgr ctrl.Manager, log logr.Logger, requeuePeriod time.Dura
 		ctrl.Log.Error(err, "unable to create controller", "controller", "HumioUser")
 		os.Exit(1)
 	}
+	if err = (&controller.HumioViewPermissionRoleReconciler{
+		Client: mgr.GetClient(),
+		CommonConfig: controller.CommonConfig{
+			RequeuePeriod: requeuePeriod,
+		},
+		HumioClient: humio.NewClient(log, userAgent),
+		BaseLogger:  log,
+	}).SetupWithManager(mgr); err != nil {
+		ctrl.Log.Error(err, "unable to create controller", "controller", "HumioViewPermissionRole")
+		os.Exit(1)
+	}
 }
